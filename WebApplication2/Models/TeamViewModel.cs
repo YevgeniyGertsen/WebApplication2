07f910e7323a9019a94dd60af7a177a7f5d1c581
YevgeniyGertsen <gersen.e.a@gmail.com>
2025-02-04 10:47:57 +0500

    add Team

---
+++ WebApplication2/Models/TeamViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models
+{
+    public class TeamViewModel
+    {
+        public string TeamImage { get; set; }
+        public PictireSize PictireSize { get; set; }
+        public string FullName { get; set; }
+        public string Position { get; set; }
+        public string Description { get; set; }
+        public string[] TeamLinks { get; set; }
+    }
+}
